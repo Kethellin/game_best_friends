8d76fc7b34372a6e5368b66340a8ea99a4ba6d85
KethellinPereira <kethellin.pereira@bitzen.com.br>
2025-03-02 20:13:54 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const messages = [
+    "Tem certeza?",
+    "Certeza mesmo?",
+    "Tá certa disso?",
+    "Anne...",
+    "Pense bem na sua resposta!",
+    "Se você disser que não, eu vou ficar muito triste...",
+    "Tá locona?",
+    "Eu vou ficar muito, muito triste...",
+    "Ok, tudo bem, vou parar de perguntar...",
+    "Tava brincando, diz sim por favor! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton  = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+
+    noButton.textContent = messages[messageIndex];
+
+    messageIndex      = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
